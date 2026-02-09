bf6a2256c1edcfbaf5975b7116d41402c4734770
Johannes Loehnert <loehnert.kde@gmx.net>
2026-02-09 21:02:11 +0100

    fix typehinting of Settings
    Move MQTT alias to the bottom, so that mqtt field has correct type.

--- ha_mqtt_device/__init__.py
+++ ha_mqtt_device/__init__.py
@@ -128,9 +128,6 @@ EntityType = TypeVar("EntityType", bound=EntityInfo)
 
 @dc.dataclass
 class Settings(Generic[EntityType]):
-    # Legacy alias for MQTT settings, so that user code keeps working.
-    class MQTT(MQTT): pass
-
     mqtt: MQTT
     """Connection to MQTT broker"""
     entity: EntityType
@@ -139,6 +136,9 @@ class Settings(Generic[EntityType]):
     manual_availability: bool = False
     """If true, the entity `availability` inside HA must be manually managed
     using the `set_availability()` method"""
+
+    # Legacy alias for MQTT settings, so that user code keeps working.
+    class MQTT(MQTT): pass
 
 
 class Discoverable(Generic[EntityType]):
